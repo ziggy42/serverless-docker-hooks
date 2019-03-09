800cb08333022d5c904a354720f505d84ca23fe3
AndreaPivetta <vanpivix@gmail.com>
2019-03-09 18:40:08 +0100

    Pull image only if it's not available locally

--- src/docker.js
+++ src/docker.js
@@ -4,14 +4,27 @@ const { normalizeBinds, normalizeImageName } = require('./utils');
 const docker = new Docker();
 
 const pullImage = async (image) => {
-    const stream = await docker.pull(normalizeImageName(image));
+    const stream = await docker.pull(image);
     await new Promise((resolve, reject) => {
         docker.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
     });
 };
 
+const isImageLocal = async (image) => {
+    const localImages = await docker.listImages({
+        filters: { reference: [image] }
+    });
+
+    return localImages.length > 0;
+};
+
 const runContainer = async ({ image, command, volumes }) => {
-    await pullImage(image);
+    const imageName = normalizeImageName(image);
+    const imageLocal = await isImageLocal(imageName);
+    if (!imageLocal) {
+        await pullImage(imageName);
+    }
+
     await docker.run(image, Array.isArray(command) ? command : [command], process.stdout, {
         HostConfig: {
             AutoRemove: true,
